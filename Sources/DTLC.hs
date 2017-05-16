20989ef1758ea85501493f4b57f765d2fc156072
Jaden Geller <jaden.geller@gmail.com>
2017-05-16 12:46:36 -0700

    Add annotations

--- Sources/DTLC.hs
+++ Sources/DTLC.hs
@@ -15,24 +15,27 @@ data Term = Type
           | Pi (Bind (Name Term, Embed Type) Term)
           | Var (Name Term) -- name indexed by what they refer to
           | App Term Term
-          | Lambda (Bind (Name Term, Embed Type) Term)
+          | Lambda (Bind (Name Term, Embed Type) Term) -- TODO: Remove annotation here, add annotation term.
+          | Annot Term Type
           -- TODO: Add holes! Will probably require backtracking unification.
 type Type = Term
       
 --- Indexing
 
-data IndexStep = AppFunc | AppArg | BindingType | BindingBody
+data IndexStep = AppFunc | AppArg | BindingType | BindingBody | AnnotTerm | AnnotType
   deriving Show
 type Index =  [IndexStep]
 
 (!!) :: LFresh m => Term -> Index -> m Term
 (!!) term [] = return term
-(!!) (App func _)  (AppFunc:index)     = func !! index
-(!!) (App _ arg)   (AppArg:index)      = arg !! index
-(!!) (Pi term)     (BindingType:index) = lunbind term $ \((_, unembed -> varType), _) -> varType !! index
-(!!) (Pi term)     (BindingBody:index) = lunbind term $ \((_, _), body)               -> body    !! index
-(!!) (Lambda term) (BindingType:index) = lunbind term $ \((_, unembed -> varType), _) -> varType !! index
-(!!) (Lambda term) (BindingBody:index) = lunbind term $ \((_, _), body)               -> body    !! index
+(!!) (App func _)   (AppFunc:index)     = func !! index
+(!!) (App _ arg)    (AppArg:index)      = arg !! index
+(!!) (Pi term)      (BindingType:index) = lunbind term $ \((_, unembed -> varType), _) -> varType !! index
+(!!) (Pi term)      (BindingBody:index) = lunbind term $ \((_, _), body)               -> body    !! index
+(!!) (Lambda term)  (BindingType:index) = lunbind term $ \((_, unembed -> varType), _) -> varType !! index
+(!!) (Lambda term)  (BindingBody:index) = lunbind term $ \((_, _), body)               -> body    !! index
+(!!) (Annot term _) (AnnotTerm:index)   = term !! index
+(!!) (Annot _ typ)  (AnnotType:index)   = typ !! index
 (!!) term (step:_) = error $ "index step " ++ show step ++ " not in " ++ show term
 
       
@@ -48,6 +51,7 @@ instance Subst Term Term where
  
 -- Show
 
+-- FIXME: Could be improved. Doesn't even take associativity into account.
 instance Show Term where
   show = runLFreshM . show'
       where show' :: LFresh m => Term -> m String
@@ -69,15 +73,20 @@ instance Show Term where
                 return $ "λ" ++ name2String varName 
                       ++ ":" ++ wrapIfLoose varType varTypeString 
                       ++ "." ++ bodyString
+            show' (Annot term typ) = do
+                termString <- show' term
+                typeString <- show' typ
+                return $ wrapIfLoose term termString ++ " : " ++ wrapIfLoose typ typeString
             -- show' Hole = return "_"
 
             looselyBound :: Term -> Bool
             -- looselyBound Hole      = False
-            looselyBound Type       = False
-            looselyBound (Pi _)     = True
-            looselyBound (Var _)    = False
-            looselyBound (App _ _)  = True
-            looselyBound (Lambda _) = True
+            looselyBound Type        = False
+            looselyBound (Pi _)      = True
+            looselyBound (Var _)     = False
+            looselyBound (App _ _)   = True
+            looselyBound (Lambda _)  = True
+            looselyBound (Annot _ _) = True
             
             wrapIfLoose :: Term -> (String -> String)
             wrapIfLoose term | looselyBound term = \termString -> "(" ++ termString ++ ")"
@@ -98,7 +107,7 @@ pi name typeAnnotation result = Pi $ bind boundName result
 
 (-->) :: Type -> Type -> Type
 (-->) = pi "_"
-infixr 9 -->
+infixr 1 -->
 
 var :: String -> Term
 var = Var . string2Name
@@ -127,11 +136,15 @@ type Stack = Index
           
 prettyError :: Term -> TypeError -> String
 prettyError term (TypeError index@(AppArg:index') (ExpectedTypeButFound expectedType foundType)) = 
-    "Error:\n" ++ show index ++ "\n" ++ show term ++ "\n" ++
+    "Error:\n" ++
     "    • Couldn't match expected type ‘" ++ show expectedType ++ "’ with actual type ‘" ++ show foundType ++ "’\n" ++
     "    • In the argument of ‘" ++ show term ++ "’, namely ‘" ++ (show . runLFreshM) (term !! reverse index) ++ "’\n" ++
     "    • In the expression ‘" ++ (show . runLFreshM) (term !! reverse index')
-prettyError _ _ = error "not implemented"
+prettyError term (TypeError (AnnotTerm:index') (ExpectedTypeButFound expectedType foundType)) = 
+    "Error:\n" ++
+    "    • Couldn't match expected type ‘" ++ show expectedType ++ "’ with actual type ‘" ++ show foundType ++ "’\n" ++
+    "    • In the expression ‘" ++ (show . runLFreshM) (term !! reverse index')
+prettyError a b = error ("not implemented " ++ show a ++ " ::: " ++ show b)
 -- FIXME: Implement rest
 
 elseThrowError :: Maybe a -> TypeError -> Except TypeError a
@@ -142,7 +155,7 @@ check :: Index -> Context -> Type -> Term -> LFreshMT (Except TypeError) ()
 -- check _ _ expectedType Infer = return expectedType
 check index context expectedType term = do 
     foundType <- infer index context term
-    if expectedType `aeq` foundType -- TODO: definitional eq
+    if expectedType `beq` foundType -- TODO: definitional eq
         then return ()
         else throwError $ TypeError index $ ExpectedTypeButFound expectedType foundType
 
@@ -181,6 +194,10 @@ infer index context (App func arg) = do
             check (AppArg:index) context varType arg
             return $ subst varName arg body
         _ -> throwError $ TypeError index ExpectedFunction -- TODO: args?
+        
+infer index context (Annot term typ) = do
+    check (AnnotTerm:index) context typ term
+    return typ
 
 runInfer :: Term -> Except TypeError Type
 runInfer term = runLFreshMT (infer [] [] term)
@@ -192,6 +209,7 @@ prettyRunInfer' :: Term -> IO ()
 prettyRunInfer' term = case prettyRunInfer term of
                          Left error -> putStrLn error
                          Right inferredType -> putStrLn $ show inferredType
+-- Why isn't deifnitional eq working??
 
 ---- Small-step evaluation
 
@@ -207,6 +225,7 @@ step (App (Lambda func) arg) = lunbind func $ \((varName, _), body) -> do
     return $ subst varName arg body
 step (App func arg) = (step func >>= \func' -> return $ App func' arg)
                   <|> (step arg  >>= \arg'  -> return $ App func arg')
+step (Annot term _) = return term -- TODO: This should be removed before here.
 
 reduce :: Term -> LFreshM Term
 reduce term = do
@@ -217,13 +236,34 @@ reduce term = do
 
 eval :: Term -> Term
 eval term = runLFreshM (reduce term)
---
+
+-- FIXME: This reduces unnecessarily much.
+beq :: Term -> Term -> Bool
+beq term1 term2 = runLFreshM $ beq' term1 term2
+    where beq' :: Term -> Term -> LFreshM Bool
+          beq' term1 term2 | term1 `aeq` term2 = return True
+                           | otherwise         = case (eval term1, eval term2) of
+            (App func1 arg1, App func2 arg2) -> (func1 `beq'` func2) >>= \eqFuncs ->
+                                                (arg1 `beq'` arg2)   >>= \eqArgs  ->
+                                                return $ eqFuncs && eqArgs
+            (Lambda lambda1, Lambda lambda2) -> lunbind lambda1 $ \(_, body1) ->
+                                                lunbind lambda2 $ \(_, body2) ->
+                                                body1 `beq'` body2
+            _ -> return False
 
 ---- Example
+
+idType' = pi "t" Type $
+              var "t" --> var "t"
 
 id' = lambda "t" Type $ 
           lambda "x" (var "t") $ 
               var "x"
+              
+idType'' = id' @@ Type @@ idType'
+
+idAnnot' = Annot id' idType'
+idAnnot'' = Annot id' idType''
 
 bool' = pi "t" Type $ 
             var "t" --> var "t" --> var "t"
@@ -257,13 +297,20 @@ conj' = lambda "p" Type $
                             lambda "f" (var "p" --> var "q" --> var "t") $
                                 var "f" @@ var "x" @@ var "y"
 
---
----- BoolType
---program = (Application
---              (Application
---                  (lambda "x" BoolType $
---                      lambda "y" UnitType $
---                          var "x")
---                  (BoolLiteral False))
---              Unit)
---
+
+conjType' = let Right typ = prettyRunInfer conj' in typ
+
+conjType'' = pi "p" Type $
+                pi "q" Type $
+                    var "p" --> var "q" --> and' @@ var "p" @@ var "q"
+
+conjType''' = (lambda "and" (Type --> Type --> Type) $
+                pi "p" Type $ 
+                    pi "q" Type $ 
+                        var "p" --> var "q" --> (var "and" @@ var "p" @@ var "q"))
+            @@ and'
+            
+conjAnnot' = Annot conj' conjType'
+conjAnnot'' = Annot conj' conjType''
+
+-- allowing eta-expansion makes computational equality undecidable... :<
