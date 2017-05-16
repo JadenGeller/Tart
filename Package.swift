260d7b9698686da095679980b837272ab21c2df1
Jaden Geller <jaden.geller@gmail.com>
2017-05-16 12:53:13 -0700

    Removed Swift code

--- Package.swift
+++
@@ -1,9 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "Tart",
-    targets: [],
-    dependencies: [
-//        .Package(url: "https://github.com/JadenGeller/Valence", majorVersion: 0)
-    ]
-)
